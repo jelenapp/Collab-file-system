b17c62abbab02e2ba91ccf0331a91637aad74a3a
Mihajlo <milanovic.mihajlo.8@gmail.com>
2026-02-23 20:21:15 +0100

    Added mail sender for user authentication

---
+++ backend/src/mailer/mailer.ts
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+import {EMAIL_APP_PASS, EMAIL_USER, NEXT_AUTH_URL} from "../config/config";
+
+export async function sendVerificationEmail(email: string, token: string) {
+  const verificationUrl = `${NEXT_AUTH_URL}/verify?token=${token}`;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL_USER,
+      pass: EMAIL_APP_PASS,
+    },
+  });
+
+  try {
+    const info = await transporter.sendMail({
+      from: `"RTC App" <${EMAIL_USER}>`,
+      to: email,
+      subject: "Verify your email",
+      html: `
+        <p>Zdravo,</p>
+        <p>Kliknite na link ispod da biste verifikovali svoj email:</p>
+        <a href="${verificationUrl}" style="color: #2563eb">${verificationUrl}</a>
+        <p>Hvala!</p>
+      `,
+    });
+
+    console.log("Verification email sent:", info);
+  } catch (error) {
+    console.error("Error sending email via Gmail SMTP:", error);
+    throw new Error("Email nije mogao biti poslat.");
+  }
+}
